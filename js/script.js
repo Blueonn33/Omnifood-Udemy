7da6d6239d49f51225dac2d60d9271fa1af1ccfe
Martin Marinov <m.marinov1733@gmail.com>
2025-09-07 17:27:54 +0300

    затваряне на навигацията за телефон

--- js/script.js
+++ js/script.js
@@ -23,9 +23,14 @@ allLinks.forEach(function (link) {
     if (href === "#") {
       window.scrollTo({ top: 0, behavior: "smooth" });
     }
+    // Scroll to other links
     if (href !== "#" && href.startsWith("#")) {
       const sectionEl = document.querySelector(href);
       sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      link.classList.toggle("nav-open");
     }
   });
 });
